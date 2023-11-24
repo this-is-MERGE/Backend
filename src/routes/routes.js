c0e7fe8b5ba974eb72497d40a919f095edf6da2b
banseok1216 <bansuk1216@naver.com>
2023-11-24 18:44:25 +0900

    Refactor delete_patient관련 부분을 router.delete로 메소드 이전, modified_patient 함수 이름을 modify_patient함수로 변경 후 roter.put 메소드로 이전, 기존 search_all_patient 함수 명을 search_patient로 수정후 기존 patient_page의 search_patient case부분과 병합 PatientController.js의 request받는 방식 query 및 params로 일부 수정

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -13,8 +13,10 @@ router.post("/signin",user.already_login, user.post_login);
 router.get("/signup",user.already_login,user.signup);
 router.post("/signup",user.already_login,user.post_user);
 //환자 전체 조회 창
-router.get("/patient",user.session_check,patient.search_all_patient);
+router.get("/patient",user.session_check,patient.search_patient);
+router.delete("/patient/:PATIENT_ID", user.session_check,patient.delete_patient);
+router.put("/patient/:PATIENT_ID",user.session_check,patient.modify_patient);
 //환자 검색
-router.post("/patient",user.session_check,patient.patient_page);
+router.post("/patient",user.session_check,patient.add_patient);
 router.get("/logout",user.session_check, user.logout);
 module.exports = router;
